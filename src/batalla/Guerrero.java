d1b1ba2182fba3240b2c9f484d666aae499f851b
Santiago Della Torre <sdellatorre@gmail.com>
2016-11-02 17:30:57 -0300

    test

--- src/batalla/Guerrero.java
+++ src/batalla/Guerrero.java
@@ -6,8 +6,8 @@ public abstract class Guerrero {
 		private int defensa;
 		private int nivelAtaque;
 		//private int estado; //GUERRERO UN ESTADO SI ES 1 ATACO y 0 NO ATACO PARA SABER SI PUEDE ATACAR DEPENDIENDO DEL ESTADO
-		private Armas armaPrimaria;
-		private Armas armaSecundaria;
+		//private Armas armaPrimaria;
+		//private Armas armaSecundaria;
 		private String nombre;
 		//RANDOM PERMITE TRAER UN OBJETO TIPO RANDOM new Random(); 
 		/*
